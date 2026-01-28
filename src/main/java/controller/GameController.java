4144f327862b10ecbd0b6a658ceaf6208d0404a3
un-known0 <nam0221@naver.com>
2026-01-28 18:10:31 +0900

    feat: GameController에 생성자 추가

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,8 +1,30 @@
 package controller;
 
+import model.BaseballGameJudge;
+import model.InputValidator;
+import model.RandomNumberGenerator;
+import view.InputView;
+import view.OutputView;
+
 import java.util.List;
 
 public class GameController {
+
+    private final RandomNumberGenerator randomNumberGenerator;
+    private final BaseballGameJudge baseballGameJudge;
+    private final InputValidator inputValidator;
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public GameController() {
+        this.randomNumberGenerator = new RandomNumberGenerator();
+        this.baseballGameJudge = new BaseballGameJudge();
+        this.inputValidator = new InputValidator();
+
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
 
     public void run() {
         throw new UnsupportedOperationException("Not implemented yet");
