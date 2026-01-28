2deac30a347c57fe258c12ec53885417939eae1a
un-known0 <nam0221@naver.com>
2026-01-28 17:41:04 +0900

    feat: InputView에 생성자 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,6 +1,15 @@
 package view;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class InputView {
+
+    private final BufferedReader br;
+
+    public InputView(){
+        this.br = new BufferedReader(new InputStreamReader(System.in));
+    }
 
     public String readNumber() {
         throw new UnsupportedOperationException("Not implemented yet");
