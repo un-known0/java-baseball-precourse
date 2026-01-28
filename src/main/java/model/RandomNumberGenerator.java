8b43e46ad38a70e8a1077e8d36b477bd61e43cb8
un-known0 <nam0221@naver.com>
2026-01-28 16:45:13 +0900

    feat: RandomNumberGenerator.pickNumber 구현

--- src/main/java/model/RandomNumberGenerator.java
+++ src/main/java/model/RandomNumberGenerator.java
@@ -11,7 +11,8 @@ public class RandomNumberGenerator {
     }
 
     public int pickNumber(int start, int end) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (start > end) throw new IllegalArgumentException("숫자의 범위가 올바르지 않습니다.");
+        return random.nextInt(end-start+1) + start;
     }
 
     public List<Integer> pickUniqueNumbers(int start, int end, int count) {
