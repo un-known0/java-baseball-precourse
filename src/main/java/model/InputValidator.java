b0dba37111592e59ba8969289134092239cd8a6c
un-known0 <nam0221@naver.com>
2026-01-28 17:29:32 +0900

    feat: InputValidator.parseToNumbers 구현

--- src/main/java/model/InputValidator.java
+++ src/main/java/model/InputValidator.java
@@ -1,11 +1,28 @@
 package model;
 
+import model.exception.InvalidInputException;
+
+import java.util.ArrayList;
 import java.util.List;
+
+import static model.exception.InputErrorCode.*;
 
 public class InputValidator {
 
     public List<Integer> parseToNumbers(String input) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (input.isEmpty()) {
+            throw new InvalidInputException(INVALID_INPUT_EMPTY);
+        }
+
+        List<Integer> numbers = new ArrayList<>();
+        for (char c : input.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new InvalidInputException(INVALID_INPUT_FORMAT);
+            }
+            numbers.add(c - '0');
+        }
+
+        return numbers;
     }
 
     public boolean hasNoDuplicates(List<Integer> numbers) {
